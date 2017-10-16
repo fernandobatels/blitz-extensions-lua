93c39ad5f8f93696c50db2ad4449b834821026b1
Fernando Batels <luisfbatels@gmail.com>
2017-10-16 20:06:47 -0200

    Change licence

--- extensions.lua
+++ extensions.lua
@@ -2,18 +2,6 @@
 --  Extensions from lua api - Blitz Extensions
 --  Copyright (C) 2016 - Fernando Batels <luisfbatels@gmail.com>
 
---  This program is free software: you can redistribute it and/or modify
---  it under the terms of the GNU General Public License as published by
---  the Free Software Foundation, either version 3 of the License.
-
---  This program is distributed in the hope that it will be useful,
---  but WITHOUT ANY WARRANTY; without even the implied warranty of
---  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---  GNU General Public License for more details.
-
---  You should have received a copy of the GNU General Public License
---  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 function string:split(separator, max, b_regexp)
 	assert(separator ~= '')
 	assert(max == nil or max >= 1)
